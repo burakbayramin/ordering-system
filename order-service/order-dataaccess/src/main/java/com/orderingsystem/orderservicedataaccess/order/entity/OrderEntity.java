e708598ebc761c3a4477b83e2416be772d18ddab
burakbayramin <burakbaydevs@gmail.com>
2024-10-28 15:41:09 +0300

    order dataacces order entity classes added

---
+++ order-service/order-dataaccess/src/main/java/com/orderingsystem/orderservicedataaccess/order/entity/OrderEntity.java
@@ -0,0 +1,37 @@
+package com.orderingsystem.orderservicedataaccess.order.entity;
+
+import com.orderingsystem.valueobject.OrderStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
+@Entity
+public class OrderEntity {
+
+    @Id
+    private UUID id;
+    private UUID customerId;
+    private UUID shopId;
+    private UUID trackingId;
+    private BigDecimal price;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    private String failureMessages;
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private OrderAddressEntity address;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItemEntity> items;
+}
